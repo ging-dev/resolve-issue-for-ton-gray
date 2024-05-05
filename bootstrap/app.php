21b969864a74c77c24944e7fc4d9ce63872681d2
ging-dev <thanh1101dev@gmail.com>
2024-05-05 20:53:00 +0700

    middleware way

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\BoTonCanMiddlewareLamHa;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,8 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // hello bé tôn gray ;))
-        $middleware->redirectGuestsTo(fn () => route('auth.redirect'));
+        $middleware->alias(['auth' => BoTonCanMiddlewareLamHa::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
